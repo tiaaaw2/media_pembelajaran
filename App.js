2b967ce3b9bb8e24be66fed9f163b025a421a759
tiaaaw2 <139294462+tiaaaw2@users.noreply.github.com>
2026-01-27 04:49:27 +0800

    update

---
+++ App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './src/Login';
+
+const Stack = createNativeStackNavigator();
+function RootStack() {
+  return (
+    // pindah pindah tab
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
